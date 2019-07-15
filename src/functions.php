4868f25c0b7d6ce725b8835f98ec565732d9ec75
Jakob Wankel <jw@envire.de>
2019-07-15 16:49:26 +0200

    Update of functions
    New function to query one value from database. New function for menu structure.

--- src/functions.php
+++ src/functions.php
@@ -9,6 +9,30 @@ function createCode($no) {
 	return $word;
 }
 
+function query_one($sql) {
+    global $pdo;
+    $row = $pdo->query( $sql )->fetch();
+    return $row[0];
+}
+
+function createAdminMenu() {
+    
+    $currentPage = basename($_SERVER['PHP_SELF']);
+    
+    $elements = array();
+    $elements[] = array('text' => 'Create topic', 'href' => 'panel.php', 'icon' => 'comments', 'active' => ($currentPage == 'panel.php') ? 1 : 0);
+    $elements[] = array('text' => 'User view', 'href' => 'view_user_topics.php', 'icon' => 'users', 'active' => ($currentPage == 'view_user_topics.php') ? 1 : 0);
+    $elements[] = array('text' => 'Attachments', 'href' => 'attachments.php', 'icon' => 'album', 'active' => ($currentPage == 'attachments.php') ? 1 : 0);
+    $elements[] = array('text' => 'Groups', 'href' => 'groups.php', 'icon' => 'users', 'active' => ($currentPage == 'groups.php') ? 1 : 0);
+    
+    $tempEl = array();
+    $tempEl[] = array('text' => 'Create', 'href' => 'user_create.php', 'active' => ($currentPage == 'user_create.php') ? 1 : 0);
+    $tempEl[] = array('text' => 'Modify', 'href' => 'user_modify.php', 'active' => ($currentPage == 'user_modify.php') ? 1 : 0);
+    $elements[] = array('text' => 'Users', 'icon' => 'album', 'group' => 1, 'subElements' => $tempEl);                  
+    
+    return $elements;
+}
+
 function toastFeedback($state, $text, $title) {
 	return array('state' => $state, 'text' => $text, 'title' => $title);
 }
